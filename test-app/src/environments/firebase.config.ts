d84017bbdf8455f424e4ab3f7802bb39a23725a0
tglasser15 <tglasser15@my.whitworth.edu>
2016-11-23 17:31:17 -0800

    delete old files

--- test-app/src/environments/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyA4BphCCTlUX9-2sv0AIfUztQgqSxEYzyc",
-  authDomain: "pridedata-20826.firebaseapp.com",
-  databaseURL: "https://pridedata-20826.firebaseio.com",
-  storageBucket: "pridedata-20826.appspot.com"
-};

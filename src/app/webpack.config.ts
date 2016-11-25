3f1e079cc9337f12c3378fdf90146a8112834687
tglasser15 <tglasser15@my.whitworth.edu>
2016-11-25 14:43:05 -0800

    awesome-ts-loader

---
+++ src/app/webpack.config.ts
@@ -0,0 +1,20 @@
+module.exports = {
+
+  // Currently we need to add '.ts' to the resolve.extensions array.
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  // Source maps support ('inline-source-map' also works)
+  devtool: 'source-map',
+
+  // Add the loader for .ts files.
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+};
